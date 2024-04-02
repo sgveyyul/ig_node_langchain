356cb29a9e7fc13b8fa9d9735b2e13997cc6ec74
Yul Stewart C Gurrea <Yul.Stewart.Gurrea@ph.ey.com>
2024-04-02 09:16:56 +0800

    updated gpt contoller

--- controllers/gpt.js
+++ controllers/gpt.js
@@ -44,9 +44,9 @@ exports.gpt = async (req, res) => {
 
   const embeddingsModel = new OpenAIEmbeddings({
     openAIApiKey: process.env.OPENAI_API_KEY, 
-    // batchSize: 512,
-    modelName: "text-embedding-ada-002",
-    // dimensions: 512
+    batchSize: 512,
+    modelName: "text-embedding-3-small",
+    dimensions: 512
   });
   
   const template = "You are a professional accountant. Answer the query in a professional way. {question}"
