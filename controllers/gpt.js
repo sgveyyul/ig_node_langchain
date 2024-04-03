1da1a94cd6bdb66739f0cfeb6845e84c98f14f16
Yul Stewart C Gurrea <Yul.Stewart.Gurrea@ph.ey.com>
2024-04-03 18:37:44 +0800

    updated gpt handleDocumentChain

--- controllers/gpt.js
+++ controllers/gpt.js
@@ -87,7 +87,7 @@ const handlePrompTemplatesChain = async(conversation) => {
   return result
 }
 
-const handleDocumentChain = async(prompt, conversation, docs) => {
+const handleDocumentChain = async(conversation, docs) => {
   const prompt = ChatPromptTemplate.fromMessages([
     [
       "system",
