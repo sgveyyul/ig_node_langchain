cac297726514beb56f2addd37f5439e7680aa810
Yul Stewart C Gurrea <Yul.Stewart.Gurrea@ph.ey.com>
2024-04-04 16:17:13 +0800

    added text to speech and speech to text

--- controllers/gpt.js
+++ controllers/gpt.js
@@ -37,7 +37,7 @@ const { Readable } = require("stream");
 
 exports.gpt = async (req, res) => {
   let file = req.file
-  let chatMessages = req.body.chatMessages || req.body;
+  let chatMessages = req.body.chatMessages;
   console.log('chatMessages', chatMessages)
 
   const lastMessageIndex = chatMessages.length - 1;
