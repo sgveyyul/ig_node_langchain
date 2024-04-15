6f1a34e5338304c33742e95d2630d231421b130c
Yul Stewart C Gurrea <yul.stewart.gurrea@ph.ey.com>
2024-04-15 21:55:04 +0800

    updated prompt for sending email

--- cron.js
+++ cron.js
@@ -5,7 +5,7 @@ const { bsp_agent_2 } = require('./agents/bsp_issuance_2')
 
 exports.run_cron = async() => {
 	// cron.schedule('0 8,16 * * *', function () {
-	cron.schedule("*/15 * * * * *", function () {
+	cron.schedule("*/60 * * * * *", function () {
 		console.log('bsp_issuance_2()')
 		bsp_agent_2()
 	});
