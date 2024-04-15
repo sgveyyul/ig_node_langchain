e590e3199dae6014fadf54c5a8c6e7bc9549c205
Yul Stewart C Gurrea <yul.stewart.gurrea@ph.ey.com>
2024-04-16 00:09:07 +0800

    updated prompts

--- agents/bsp_issuance_2.js
+++ agents/bsp_issuance_2.js
@@ -113,7 +113,7 @@ exports.bsp_agent_2 = async() => {
 
     const input2 = `Lets name the list above list A and this list of objects as list B ${JSON.stringify(existing_bsp.data, null, 2)}. 
     Can you compare list A and B using their keys number and date_issued, and get the elements that are in list A but not in list B.
-    Create a list of objects for it and lets call it List C. The keys of the object are number, date_issued, subject and url. If there are none,
+    Create a list of objects for it and lets call it List C. The keys of the object are number, date_issued, subject and url. If list C is empty,
     just say list C is empty.
     .`
 		const result2 = await executorWithMemory.invoke({
