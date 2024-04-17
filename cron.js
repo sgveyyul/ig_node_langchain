5bc0b83b2a76dd81e1a02764f15f23de6bcecec9
Yul Stewart C Gurrea <yul.stewart.gurrea@ph.ey.com>
2024-04-17 09:22:09 +0800

    updated prompt

--- cron.js
+++ cron.js
@@ -4,10 +4,11 @@ const { bsp_agent_2 } = require('./agents/bsp_issuance_2')
 
 
 exports.run_cron = async() => {
+	bsp_agent_2()
 	// cron.schedule('0 8,16 * * *', function () {
-	cron.schedule("*/60 * * * * *", function () {
-		console.log('bsp_issuance_2()')
-		bsp_agent_2()
-	});
+	// cron.schedule("*/60 * * * * *", function () {
+	// 	console.log('bsp_issuance_2()')
+	// 	bsp_agent_2()
+	// });
 	
 }
