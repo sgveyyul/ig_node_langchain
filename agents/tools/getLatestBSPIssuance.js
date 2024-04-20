69bc56ed4ced471778988c8735d1778fefb94aa4
Yul Stewart C Gurrea <yul.stewart.gurrea@ph.ey.com>
2024-04-20 21:12:30 +0800

    updated prompt

--- agents/tools/getLatestBSPIssuance.js
+++ agents/tools/getLatestBSPIssuance.js
@@ -25,8 +25,8 @@ exports.getLatestBSPIssuance = async () => {
     func: async ({ bsp_arr }) => {
       console.log('bsp_arr', bsp_arr)
       const existing_bsp_issuances = await BSPRegulations.listAll()
+      console.log('existing_bsp_issuances', existing_bsp_issuances)
       if(existing_bsp_issuances.data.length === 0) {
-        console.log('existing_bsp_issuances', existing_bsp_issuances)
         return `Here are the new bsp issuances ${JSON.stringify(bsp_arr, null, 2)}.`
       }
       console.log('existing_bsp_issuances', existing_bsp_issuances.data)
